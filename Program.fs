d554318271e72b9ac275bdb0abf9b5b58ad19a46
Tatamo <nsorder@gmail.com>
2018-12-21 05:00:47 +0900

    implement applications of lambda

--- Program.fs
+++ Program.fs
@@ -18,34 +18,16 @@ type Exp =
   | IsZero of Exp
   | If of Exp * Exp * Exp
   | App of Exp * Exp
-  | Lambda of string * Type * Exp
+  | Lambda of string * Exp
   | Error
   // UNDONE
   // | Fix of string * Type * Exp
 
-let expression =
-  Lambda(
-    "x",
-    Num,
-    If(
-      IsZero(Var("x")),
-      Zero,
-      App(
-        Lambda(
-          "y",
-          Num,
-          Succ(Succ(Var("y")))
-        ),
-        Var("x")
-      )
-    )
-  )
-
 type Env = string -> Exp
 let emptyEnv (s: string) = Error
 
-let update env name value : Env =
-  fun s -> if s = name then value else env(s);
+let update env name exp : Env =
+  fun s -> if s = name then exp else env(s);
 
 let rec parse env exp =
   match exp with
@@ -74,12 +56,29 @@ let rec parse env exp =
       | True -> parse env e2
       | False -> parse env e3
       | _ -> Error
-  // 以下未実装
-  (*
-  | App(e1, e2) ->
+  | App(e1,e2) ->
     match parse env e1 with
-      | Function(name,exp) -> parse env e2
-      | _ -> Error
-  | Lambda(name,t,exp) -> Function(name,exp)
-  *)
+    | Lambda(name,exp) -> parse (update env name (parse env e2)) exp
+    | _ -> Error
+  | Lambda(name,exp) -> Lambda(name,exp)
   | _ -> Error
+
+let expression =
+  App(
+    Lambda(
+      "x",
+      If(
+        IsZero(Var("x")),
+        Zero,
+        App(
+          Lambda(
+            "y",
+            Succ(Succ(Var("y")))
+          ),
+          Var("x")
+        )
+      )
+    ),
+    Succ(Zero)
+  )
+printfn "%A" (parse emptyEnv expression);
