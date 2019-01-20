8a1000dfcd6d9a25789d5143daec0b8b2e5a01d0
Tatamo <nsorder@gmail.com>
2019-01-20 20:45:38 +0900

    add type check when reduce terms

--- reduction.fs
+++ reduction.fs
@@ -1,6 +1,7 @@
 module Reduction
 open System
 open Exp
+open CheckType
 
 // Exp中に出現する自由変数名の集合を得る
 let getFreeVariables = 
@@ -17,30 +18,31 @@ let getFreeVariables =
 let rec getNewVariableName fv baseName =
   if Set.contains baseName fv then getNewVariableName fv (String.Format("{0}'", baseName)) else baseName
 
-// TODO: 型チェック
-let rec substitute exp1 v exp2 =
-//  printfn "substitute %A[%A:=%A]" exp1 name exp2
-  let mapLambda _ v exp2 (v', e) =
-    if fst v' = fst v then (v', e) else
-    let fv1 = getFreeVariables e in
-    let fv2 = getFreeVariables exp2 in
-    if Set.contains (fst v) fv1 && Set.contains (fst v') fv2 then
-      let freshName = getNewVariableName (Set.union fv1 fv2) (fst v') in
-      ((freshName, (snd v')), substitute (substitute e v' (Var freshName)) v exp2)
-    else
-      (v', substitute e v exp2)
-    in
-  match exp1 with
-  | True | False | Zero | Error(_)-> exp1
-  | Succ(e) -> Succ(substitute e v exp2)
-  | Pred(e) -> Pred(substitute e v exp2)
-  | IsZero(e) -> IsZero(substitute e v exp2)
-  | If(e1,e2,e3) -> If(substitute e1 v exp2, substitute e2 v exp2, substitute e3 v exp2)
-  | Var(name') -> if name' = fst v then exp2 else Var(name')
-  | App(e1,e2) -> App(substitute e1 v exp2, substitute e2 v exp2)
-  | Lambda(v' ,e) -> Lambda (mapLambda exp1 v exp2 (v', e))
-  | Fix(v', e) -> Fix (mapLambda exp1 v exp2 (v', e))
-
+let substitute exp1 v exp2 =
+  let rec substitute exp1 v exp2 =
+  //  printfn "substitute %A[%A:=%A]" exp1 name exp2
+    let mapLambda _ v exp2 (v', e) =
+      if fst v' = fst v then (v', e) else
+      let fv1 = getFreeVariables e in
+      let fv2 = getFreeVariables exp2 in
+      if Set.contains (fst v) fv1 && Set.contains (fst v') fv2 then
+        let freshName = getNewVariableName (Set.union fv1 fv2) (fst v') in
+        ((freshName, (snd v')), substitute (substitute e v' (Var freshName)) v exp2)
+      else
+        (v', substitute e v exp2)
+      in
+    match exp1 with
+    | True | False | Zero | Error(_)-> exp1
+    | Succ(e) -> Succ(substitute e v exp2)
+    | Pred(e) -> Pred(substitute e v exp2)
+    | IsZero(e) -> IsZero(substitute e v exp2)
+    | If(e1,e2,e3) -> If(substitute e1 v exp2, substitute e2 v exp2, substitute e3 v exp2)
+    | Var(name') -> if name' = fst v then exp2 else Var(name')
+    | App(e1,e2) -> App(substitute e1 v exp2, substitute e2 v exp2)
+    | Lambda(v' ,e) -> Lambda (mapLambda exp1 v exp2 (v', e))
+    | Fix(v', e) -> Fix (mapLambda exp1 v exp2 (v', e))
+  in
+  if checkType exp2 emptyEnv = snd v then Some(substitute exp1 v exp2) else None
 
 let rec reduce exp =
   match exp with
@@ -78,12 +80,16 @@ and reduceIf exp1 exp2 exp3 =
   | False -> exp3
   | _ -> If(reduce exp1, exp2, exp3)
 and reduceApp exp1 exp2 =
-  // TODO: 型チェック
   match exp1 with
-  | Lambda(v, exp) -> substitute exp v exp2
+  | Lambda(v, exp) -> 
+    match substitute exp v exp2 with
+    | Some(e) -> e
+    | None -> Error(Some("TypeError"))
   | _ -> App(reduce exp1, exp2)
 and reduceFix v exp =
-  substitute exp v (Fix(v,exp))
+  match substitute exp v (Fix(v,exp)) with
+  | Some(e) -> e
+  | None -> Error(Some("TypeError"))
 
 let rec reduceAll exp =
   if isValue exp then exp else reduceAll (reduce exp)
